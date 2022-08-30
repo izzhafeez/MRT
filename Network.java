7e11d5ae79a1417bdebb1f06db46a01a229bf4f6
Hafeez <izzhafeez@gmail.com>
2022-08-30 21:38:23 +0800

    Edited ./Network.java

--- Network.java
+++ Network.java
@@ -1,22 +1,39 @@
 import java.util.HashMap;
 class Network {
   
-  HashMap<String, Line> lines;
+  private HashMap<String, Line> lines;
+  private HashMap<String, Platform> platforms;
+  private HashMap<String, Station> stations;
   
   public Network() {
-    lines = new HashMap<String, Line>();
+    this.lines = new HashMap<String, Line>();
+    this.platforms = new HashMap<String, Platform>();
+    this.stations = new HashMap<String, Station>();
   }
 
   private void addLine(Line line) {
-    lines.put(line.toString(), line);
+    this.lines.put(line.toString(), line);
   }
 
-  public void addStation(Station station) {
-     
-  }
+  public void addPlatform(
+    String lineName,
+    String stationName,
+    int platformNumber,
+    String suffix) {
 
-  public void removeStation(Station station) {
-    
+    Line line = this.lines.get(lineName);
+    PlatformCode platformCode = new PlatformCode(line, platformNumber, suffix);
+    Platform platform = new Platform(platformCode);
+    this.platforms.put(platform.toString(), platform);
+
+    if (this.stations.containsKey(stationName)) {
+      Station station = this.stations.get(stationName);
+      station.addPlatform(platform);
+    } else {
+      Station station = new Station(stationName);
+      station.addPlatform(platform);
+      this.stations.put(station.getName(), station);
+    }
   }
 
 }
