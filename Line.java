9368a49f3275eacc355f5abbe850a2b202deca94
Hafeez <izzhafeez@gmail.com>
2022-08-30 21:38:22 +0800

    Edited ./DLL.java

---
+++ Line.java
@@ -0,0 +1,9 @@
+public class Line {
+  public String prefix;
+  public int maxNumber;
+
+  public Line(String prefix, int maxNumber) {
+    this.prefix = prefix;
+    this.maxNumber = maxNumber;
+  }
+}
